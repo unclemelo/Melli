314d9acc347373290d5609cee503ee09165fcb14
Not-Melo <2024ben.johnson@gmail.com>
2024-11-19 01:42:54 -0600

    revert back

--- cogs/automod.py
+++ cogs/automod.py
@@ -52,8 +52,10 @@ class AutoMod(commands.Cog):
                 ),
                 actions=[
                     discord.AutoModRuleAction(
-                        channel_id=channel.id,
                         type=discord.AutoModRuleActionType.block_message
+                    ), 
+                    discord.AutoModRuleAction(
+                        channel_id=channel.id
                     )],
                 enabled=True,
                 reason="Prevent harmful or spam links."
@@ -69,8 +71,10 @@ class AutoMod(commands.Cog):
                 ),
                 actions=[
                     discord.AutoModRuleAction(
-                        channel_id=channel.id,
                         type=discord.AutoModRuleActionType.block_message
+                    ), 
+                    discord.AutoModRuleAction(
+                        channel_id=channel.id
                     )],
                 enabled=True,
                 reason="Prevent spam or malicious text."
