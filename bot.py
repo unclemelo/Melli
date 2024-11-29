d422625c299d59da0e3abbfc4d76d599085015a8
Not-Melo <2024ben.johnson@gmail.com>
2024-11-29 03:54:23 -0600

    status update

--- bot.py
+++ bot.py
@@ -39,8 +39,7 @@ async def update_server_count():
             status=discord.Status.dnd,
             activity=discord.Activity(
                 type=discord.ActivityType.watching,
-                name=f"🦃 | Happy Thanksgiving"
-                # {len(client.guilds)} guilds!
+                name=f"🍉 | {len(client.guilds)} guilds!"
             )
         )
     except Exception as e:
