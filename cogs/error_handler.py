15b96ef254556804b7f0fb82970182acfafb62db
Not-Melo <2024ben.johnson@gmail.com>
2024-12-01 08:10:06 -0600

    new error handler

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,6 +1,7 @@
 import discord
 import datetime
 import pytz
+import traceback
 from discord import app_commands, Interaction
 from discord.ext import commands
 
@@ -13,32 +14,56 @@ class ERROR(commands.Cog):
     async def global_app_command_error(
         self, interaction: Interaction, error: Exception
     ):
+        # Gather error details
+        error_type = type(error).__name__
+        traceback_details = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+
         embed = discord.Embed(
             title="Command Error",
             description=f"An error occurred in the following command: `{interaction.command}`",
             color=discord.Color.red(),
         )
+        embed.add_field(name="Error Type", value=f"`{error_type}`", inline=False)
         embed.add_field(
-            name="Error",
+            name="Error Message",
             value=f"```ansi\n[2;31m{str(error)}\n[0m[2;31m[0m```",
             inline=False,
         )
-        embed.set_thumbnail(
-            url="https://cdn.discordapp.com/attachments/1255215236370141236/1255216537376129127/1200px-No_icon_28white_X_on_red_circle29.png?ex=667c52ff&is=667b017f&hm=91371407f058f63b6094ea4f35e5f94f704c23f24addd83e3b128bbeb96a6b6a&"
+        embed.add_field(
+            name="Traceback",
+            value=f"```py\n{traceback_details[:1024]}```",  # Truncate if too long
+            inline=False,
         )
 
+        if interaction.user:
+            embed.add_field(
+                name="User",
+                value=f"Name: `{interaction.user}`\nID: `{interaction.user.id}`",
+                inline=False,
+            )
+
+        if interaction.guild:
+            embed.add_field(
+                name="Guild",
+                value=f"Name: `{interaction.guild.name}`\nID: `{interaction.guild.id}`",
+                inline=False,
+            )
+
         utc_time = datetime.datetime.utcnow()
-
         target_timezone = pytz.timezone('America/Chicago')
-
         cst_time = utc_time.replace(tzinfo=pytz.utc).astimezone(target_timezone)
 
         embed.set_footer(text=f"{cst_time.strftime('%m/%d/%Y | %H:%M%p [CST]')}")
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/attachments/1255215236370141236/1255216537376129127/1200px-No_icon_28white_X_on_red_circle29.png"
+        )
+
+        # Send to the specified error channel
         error_channel = self.bot.get_channel(self.error_channel_id)
         if error_channel:
             await error_channel.send(embed=embed)
         else:
-            print(f"Error: {error}")
+            print(f"Error occurred but could not send to channel:\n{traceback_details}")
 
     @app_commands.command(name="simulate_error", description="Testing errors")
     @app_commands.checks.has_permissions(administrator=True)
