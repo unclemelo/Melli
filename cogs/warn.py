c8ec94a5b41c0d1caf2b98a9207a7b88322e9522
Not-Melo <2024ben.johnson@gmail.com>
2025-01-20 07:53:06 -0600

    patch

--- cogs/warn.py
+++ cogs/warn.py
@@ -1,8 +1,8 @@
 import discord
 import json
 import os
+from discord import app_commands
 from discord.ext import commands
-from discord import Embed
 
 WARN_FILE = 'data/warns.json'  # File to store warnings
 
