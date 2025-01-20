885ab4f3d7570d0f05ffd65e2f603da566d776ce
Pitr1010 <peto.jagercik@centrum.sk>
2025-01-20 22:19:31 +0100

    test2

--- cogs/automod.py
+++ cogs/automod.py
@@ -68,16 +68,21 @@ class AutoMod(commands.Cog):
 
                 ##Test
                 try:
-                    print("TEST")
+                    print(f"TEST")
                     automod_rule = await guild.create_automod_rule(
                         name=name,
                         event_type=discord.AutoModRuleEventType.message_send,
-                        trigger=discord.AutoModTrigger(
-                            type=discord.AutoModRuleTriggerType.keyword,
-                            regex_patterns=regex_patterns,
-                            keyword_filter=blocked_words,
-                            allow_list=allowed_links
-                        ),
+                        try:
+                            print(f"TEST 2")
+                            trigger=discord.AutoModTrigger(
+                                type=discord.AutoModRuleTriggerType.keyword,
+                                regex_patterns=regex_patterns,
+                                keyword_filter=blocked_words,
+                                allow_list=allowed_links
+                            ),
+                        except:
+                            print(f"TEST 2 FAILED")
+
                         actions=[
                             discord.AutoModRuleAction(
                                 channel_id=channel.id,
@@ -90,7 +95,7 @@ class AutoMod(commands.Cog):
 
                     created_rules.append(automod_rule.name)
                 except:
-                    print("TEST FAILED")
+                    print(f"TEST FAILED")
 
             embed = discord.Embed(
                 title="AutoMod setup complete!",
