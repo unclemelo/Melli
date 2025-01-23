3033e47315f741a6a1ceb5960aef2f0ea92e6013
Not-Melo <2024ben.johnson@gmail.com>
2025-01-23 00:27:09 -0600

    fix inline

--- cogs/credits.py
+++ cogs/credits.py
@@ -13,9 +13,9 @@ class Credits(commands.Cog):
     async def creditscmd(self, interaction: discord.Interaction):
         try:            
             embed = discord.Embed(title="Credits", color=0x99AAb5)
-            embed.add_field(name="Developers", value="`@_uncle_melo_`, `@pitr1010`, `@illtana`", inline=True)
-            embed.add_field(name="Editor", value="`@mizuki_mochizuki2090`", inline=True)
-            embed.add_field(name="Artists", value="`@soupinascoop`, `@bunnnl`", inline=True)
+            embed.add_field(name="Developers", value="`@_uncle_melo_`, `@pitr1010`, `@illtana`", inline=False)
+            embed.add_field(name="Editor", value="`@mizuki_mochizuki2090`", inline=False)
+            embed.add_field(name="Artists", value="`@soupinascoop`, `@bunnnl`", inline=False)
             await interaction.response.send_message(embed=embed)
         except Exception as e:
             await interaction.response.send_message(f"An error occurred. Please contact an administrator.\n```{e}```", ephemeral=True)
