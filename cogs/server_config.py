cc893b4e07b746d5363df202b1be0b2fd1433941
Melo <2024ben.johnson@gmail.com>
2025-02-25 05:50:23 -0600

    Currently under maintenance.

--- cogs/server_config.py
+++ cogs/server_config.py
@@ -77,6 +77,7 @@ class ServerConfig(commands.Cog):
         embed = discord.Embed(title="Command Configuration",
                               description="Select commands to toggle on/off for this server.",
                               color=discord.Color.blue())
+        embed.get_footer("Currently under maintenance.")
         await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
 
 async def setup(bot: commands.Bot):
